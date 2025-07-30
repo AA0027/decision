4417a7ec26188e887bba5751dcfb63e4c0237c72
창성 조 <tmxk1277@outlook.kr>
2025-07-30 14:53:34 +0900

    UI 및 기능 완성 => 다음, 이전 프레임 이벤트 처리 필요

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,4 @@
 #include "mainwindow.h"
-
-#include <QMessageBox>
 #include <QTimer>
 
 
@@ -10,10 +8,25 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent),
 
     createMenuBar();
     createUI();
+    setupTimer();
+
+    msgBox.setStyleSheet(
+        "QMessageBox {"
+        "    min-width: 400px;"
+        "    min-height: 200px;"
+        "}"
+        "QMessageBox QLabel {"
+        "    font-size: 14px;"
+        "    font-weight: bold;"
+        "    color: #333333;"
+        "    qproperty-alignment: AlignCenter;"  // 가운데 정렬
+        "    min-width: 300px;"
+        "}"
+        );
 }
 
 // slots
-// 폴더 열기 선택시
+// 폴더 열기 선택시 (slots)
 void MainWindow::selectFolder()
 {
     QString path;
@@ -21,12 +34,18 @@ void MainWindow::selectFolder()
     {
         path = QFileDialog::getExistingDirectory(this, "select",
                                                        QDir::homePath(), QFileDialog::ShowDirsOnly);
+        if (path.isEmpty()) {
+            return;
+        }
         recentPath = path;
     }
     else
     {
         path = QFileDialog::getExistingDirectory(this, "select",
                                                        recentPath, QFileDialog::ShowDirsOnly);
+        if (path.isEmpty()) {
+            return;
+        }
         recentPath = path;
     }
 
@@ -34,7 +53,10 @@ void MainWindow::selectFolder()
 
     if(folderList.size() > 4)
     {
-        QMessageBox::warning(this, "경고", "폴더추가 불가");
+        msgBox.setWindowTitle("경고");
+        msgBox.setText("폴더추가 불가");
+        msgBox.exec();
+
         return;
     }
 
@@ -42,6 +64,7 @@ void MainWindow::selectFolder()
 
     QDir dir(path);
     QIcon folderIcon = style() -> standardIcon(QStyle::SP_DirIcon);
+    QIcon imageIcon = style() -> standardIcon(QStyle::SP_FileIcon);
     QStringList imageFiles;
     QStringList filters;
     filters << "*.jpg" << "*.jpeg" << "*.png" << "*.bmp" << "*.gif";
@@ -57,7 +80,7 @@ void MainWindow::selectFolder()
 
     for(int i = 0; i < 5; i++)
     {
-        subFiles.emplaceBack(new QStandardItem(imageFiles[i]));
+        subFiles.emplaceBack(new QStandardItem(imageIcon, imageFiles[i]));
     }
 
     subFiles.emplaceBack(new QStandardItem("..."));
@@ -65,52 +88,176 @@ void MainWindow::selectFolder()
     rootNode -> appendRow(includeFolder);
     includeFolder -> appendColumn(subFiles);
 
+    msgBox.setWindowTitle("정보");
+    msgBox.setText(QString("총 %1개의 이미지를 불러왔습니다.").arg(imageFiles.size()));
+    msgBox.exec();
 }
 
 
-// 재생할 폴더 선택
+// 재생할 폴더 선택 (slots)
 void MainWindow::preparePlay(const QModelIndex &index)
 {
-    if(!index.isValid())
+    if(!index.parent().isValid())
     {
-        QMessageBox::warning(this, "경고", "유효하지 않는 폴더입니다.");
-        return;
-    }
-
-    QStandardItem *item = viewModel -> itemFromIndex(index);
-    QString path = item -> text();
-    for(const QString& p : folderList)
-    {
-        if(p.contains(path))
+        if(!index.isValid())
         {
-            path = p;
+            msgBox.setWindowTitle("경고");
+            msgBox.setText("유효하지 않는 폴더입니다.");
+            msgBox.exec();
+
+            return;
+        }
+
+        QStandardItem *item = viewModel -> itemFromIndex(index);
+        QString path = item -> text();
+
+        for(int i = 0; i < folderList.size(); i++)
+        {
+            if(folderList[i].contains(path))
+                path = folderList[i];
+        }
+
+        QDir dir(path);
+
+        QStringList nameFilters;
+        nameFilters << "*.jpg" << "*.jpeg" << "*.png" << "*.bmp" << "*.gif" << "*.tiff";
+
+        imageList.clear();
+        imageList = dir.entryList(nameFilters, QDir::Files, QDir::Name);
+
+        if (imageList.isEmpty()) {
+            msgBox.setWindowTitle("정보");
+            msgBox.setText("선택한 폴더에 이미지 파일이 없습니다.");
+            msgBox.exec();
+
+            return;
+        }
+
+        // 전체 경로로 변환
+        for (int i = 0; i < imageList.size(); ++i) {
+            imageList[i] = dir.absoluteFilePath(imageList[i]);
+        }
+
+        currentFrame = 0;
+        slider->setEnabled(true);
+        slider->setRange(0, imageList.size() - 1);
+        slider->setValue(0);
+
+        updateFrame();
+        updateSlider();
+
+        msgBox.setWindowTitle("정보");
+        msgBox.setText("동영상 재생 준비 완료");
+        msgBox.exec();
+    }
+}
+
+// 컨텍스트 메뉴 뜨우기 (slots)
+void MainWindow::showContextMenu(const QPoint &pos)
+{
+    QModelIndex index = treeView->indexAt(pos);
+
+    // 유효한 인덱스이고, 최상위 루트 아이템인지 확인
+    if (index.isValid() && index.parent() == QModelIndex()) {
+        QMenu contextMenu(this);
+        contextMenu.addAction(deleteAction);
+
+        // 글로벌 좌표로 변환하여 메뉴 표시
+        contextMenu.exec(treeView->mapToGlobal(pos));
+    }
+}
+
+// 선택됀 폴더 삭제 (slots)
+void MainWindow::deleteFolder()
+{
+    QModelIndex currentIndex = treeView->currentIndex();
+
+    // 최상위 루트 아이템인지 다시 한번 확인
+    if (currentIndex.isValid() && currentIndex.parent() == QModelIndex()) {
+        // 사용자 확인 대화상자 (선택사항)
+        int ret = QMessageBox::question(this, "삭제 확인",
+                                        "선택한 항목을 삭제하시겠습니까?",
+                                        QMessageBox::Yes | QMessageBox::No);
+
+        if (ret == QMessageBox::Yes) {
+            // 모델에서 해당 행 삭제
+            viewModel->removeRow(currentIndex.row());
+
+            // 또는 QStandardItemModel을 사용하는 경우:
+            // QStandardItem *item = viewModel->itemFromIndex(currentIndex);
+            // if (item) {
+            //     viewModel->removeRow(item->row());
+            // }
         }
     }
 
-    playFolder = path;
 }
-
-// 동영상 재생/일시정지
+// 동영상 재생/일시정지 (slots)
 void MainWindow::controlPlay()
 {
+    if (imageList.isEmpty()) {
+        msgBox.setWindowTitle("경고");
+        msgBox.setText("먼저 이미지 폴더를 선택해주세요.");
+        msgBox.exec();
+        return;
+    }
 
+    if (isPlaying) {
+        timer->stop();
+        playBtn->setIcon(QIcon(":/play.png"));
+        isPlaying = false;
+    } else {
+        timer->start();
+        playBtn->setIcon(QIcon(":/pause.png"));
+        isPlaying = true;
+    }
 }
 
-// 다음 프레임
+// 동영상 정지 (slot)
+void MainWindow::stop()
+{
+    timer->stop();
+    currentFrame = 0;
+    isPlaying = false;
+    playBtn->setIcon(QIcon(":/play.png"));
+    slider->setValue(0);
+    updateFrame();
+}
+
+// 다음 프레임 (slots)
 void MainWindow::nextFrame()
 {
     if(!imageList.isEmpty())
     {
         currentFrame = (currentFrame + 1) % imageList.size();
         updateFrame();
-        // updateSlider();
+        updateSlider();
+    }
+}
+
+// 다음 프레임 (slots)
+void MainWindow::previousFrame()
+{
+    if (!imageList.isEmpty()) {
+        currentFrame = (currentFrame - 1 + imageList.size()) % imageList.size();
+        updateFrame();
+        updateSlider();
+    }
+}
+
+// slider 이벤트 (slots)
+void MainWindow::onSliderChanged(int value)
+{
+    if (!imageList.isEmpty() && value < imageList.size()) {
+        currentFrame = value;
+        updateFrame();
     }
 }
 
 // END slots
 
 
-
+// UI 설정 함수들
 void MainWindow::createMenuBar()
 {
     QMenuBar* menuBar = new QMenuBar(this);
@@ -119,6 +266,8 @@ void MainWindow::createMenuBar()
 
     QAction *openAction = fileMenu -> addAction("열기");
     connect(openAction, &QAction::triggered, this, &MainWindow::selectFolder);
+
+    fileMenu->addSeparator();
 
     QAction* setAction = fileMenu -> addAction("설정");
     connect(setAction, &QAction::triggered, this, &MainWindow::selectFolder);
@@ -135,8 +284,8 @@ void MainWindow::createUI()
     screenViewer();
 
 
-    layout -> addWidget(treeView, 1);
-    layout -> addWidget(screenWidget, 5);
+    layout -> addWidget(treeView);
+    layout -> addWidget(screenWidget);
 
 
     central -> setLayout(layout);
@@ -148,11 +297,20 @@ void MainWindow::fileViewer()
 
 
     treeView = new QTreeView(central);
-
+    treeView -> setMinimumWidth(200);
+    treeView -> setMaximumWidth(200);
     treeView -> setModel(viewModel);
     treeView -> setHeaderHidden(true);
 
+    treeView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    treeView->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(treeView, &QTreeView::customContextMenuRequested,
+            this, &MainWindow::showContextMenu);
     connect(treeView, &QTreeView::doubleClicked, this, &MainWindow::preparePlay);
+
+    deleteAction = new QAction("삭제",this);
+    connect(deleteAction, &QAction::triggered, this, &MainWindow::deleteFolder);
 }
 
 void MainWindow::screenViewer()
@@ -165,36 +323,58 @@ void MainWindow::screenViewer()
     videoView -> setAlignment(Qt::AlignCenter);
     videoView -> setMinimumSize(640, 480);
     videoView ->setStyleSheet("border: 1px solid gray;");
-
-
+    QHBoxLayout* bottomLayout = new QHBoxLayout();
     QHBoxLayout* controlLayout = new QHBoxLayout();
+
+    frameInfo = new QLabel("프레임: 0 / 0");
+    frameInfo -> setStyleSheet(
+        "QLabel {"
+        "    font-size: 14px;"
+        "    font-weight: bold;"
+        "}"
+        );
+
     playBtn = new QPushButton();
     playBtn -> setIcon(QIcon(":/play.png"));
-    // connect(playBtn, &QPushButton::clicked, this, &MainWindow::controlPlay);
+    connect(playBtn, &QPushButton::clicked, this, &MainWindow::controlPlay);
 
     nextBtn = new QPushButton();
     nextBtn -> setIcon(QIcon(":/next.png"));
+    connect(nextBtn, &QPushButton::clicked, this, &MainWindow::nextFrame);
 
+    previousBtn = new QPushButton();
+    previousBtn -> setIcon(QIcon(":/before.png"));
+    connect(previousBtn, &QPushButton::clicked, this, &MainWindow::previousFrame);
 
-    beforeBtn = new QPushButton();
-    beforeBtn -> setIcon(QIcon(":/before.png"));
+    stopBtn = new QPushButton();
+    stopBtn -> setIcon(QIcon(":/stop.png"));
+    connect(stopBtn, &QPushButton::clicked, this, &MainWindow::stop);
 
     customPushBtn(playBtn);
     customPushBtn(nextBtn);
-    customPushBtn(beforeBtn);
+    customPushBtn(previousBtn);
+    customPushBtn(stopBtn);
 
-    controlLayout -> addWidget(beforeBtn);
+
+    controlLayout -> addWidget(previousBtn);
     controlLayout -> addWidget(playBtn);
+    controlLayout -> addWidget(stopBtn);
     controlLayout -> addWidget(nextBtn);
 
-    controlLayout -> setAlignment(Qt::AlignCenter);
+    // controlLayout -> setAlignment(Qt::AlignCenter);
+
+    bottomLayout -> addWidget(frameInfo);
+    bottomLayout->addStretch();
+    bottomLayout -> addLayout(controlLayout);
+    bottomLayout->addStretch();
 
     slider = new QSlider(Qt::Horizontal);
     slider -> setEnabled(false);
+    connect(slider, &QSlider::valueChanged, this, &MainWindow::onSliderChanged);
 
     screenLayout -> addWidget(videoView, 5);
     screenLayout -> addWidget(slider, 2);
-    screenLayout -> addLayout(controlLayout);
+    screenLayout -> addLayout(bottomLayout);
 }
 
 void MainWindow::customPushBtn(QPushButton* btn)
@@ -213,40 +393,41 @@ void MainWindow::customPushBtn(QPushButton* btn)
         );
 }
 
-// =====================================================
+// ======================== UI 함수설정 END =============================
 
 void MainWindow::setupTimer()
 {
     timer = new QTimer(this);
-    timer->setInterval(10);
+    timer->setInterval(100);
     connect(timer, &QTimer::timeout, this, &MainWindow::nextFrame);
 }
 
-void MainWindow::loadImages()
-{
-
-}
 
 void MainWindow::updateFrame()
 {
+    if (!imageList.isEmpty() && currentFrame < imageList.size()) {
+        QPixmap pixmap(imageList[currentFrame]);
+        if (!pixmap.isNull()) {
+            // 이미지 레이블 크기에 맞게 스케일링
+            QPixmap scaledPixmap = pixmap.scaled(
+                videoView->size(),
+                Qt::KeepAspectRatio,
+                Qt::SmoothTransformation
+                );
+            videoView->setPixmap(scaledPixmap);
+        }
 
+        frameInfo->setText(
+            QString("프레임: %1 / %2").arg(currentFrame + 1).arg(imageList.size())
+            );
+    }
 }
 
 void MainWindow::updateSlider()
 {
-
+    slider->blockSignals(true);
+    slider->setValue(currentFrame);
+    slider->blockSignals(false);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,8 @@
 #include <QMenu>
 #include <QMenuBar>
 #include <QStandardItemModel>
+#include <QMessageBox>
+#include <QResizeEvent>
 #include <QtMultimediaWidgets/qvideowidget.h>
 
 
@@ -23,9 +25,16 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget *parent = nullptr);
 
+
 private slots:
     // 폴더 추가
     void selectFolder();
+
+    // 폴더 설정 메뉴 보여주기
+    void showContextMenu(const QPoint &pos);
+
+    // 산텍한 폴더 삭제
+    void deleteFolder();
 
     // 재생할 폴더 선택
     void preparePlay(const QModelIndex &index);
@@ -42,7 +51,8 @@ private slots:
     // 이전 프레임
     void previousFrame();
 
-
+    // slider 이벤트
+    void onSliderChanged(int value);
 
 
 private:
@@ -51,7 +61,7 @@ private:
 
     // 파일 트리 보여주는 widget (parent : central)
     QWidget* fileWidget;
-
+    QAction* deleteAction;
     // 비디오 재생 및 조작 widget (parent : central)
     QWidget* screenWidget;
 
@@ -64,7 +74,8 @@ private:
     QLabel* videoView;
     QPushButton* playBtn;
     QPushButton* nextBtn;
-    QPushButton* beforeBtn;
+    QPushButton* stopBtn;
+    QPushButton* previousBtn;
     QSlider* slider;
 
     QString recentPath;
@@ -73,12 +84,14 @@ private:
 
     // 동영상 재생관련 변수
     QTimer* timer;
-
-    QString playFolder;
     QStringList imageList;
+    QLabel* frameInfo;
 
     bool isPlaying;
     int currentFrame;
+
+    // 기타
+    QMessageBox msgBox;
 
 private:
     void createUI();
@@ -88,7 +101,23 @@ private:
     void customPushBtn(QPushButton* btn);
 
     void setupTimer();
-    void loadImages();
     void updateFrame();
     void updateSlider();
+
+    void resizeEvent(QResizeEvent* event) override
+    {
+        if (!imageList.isEmpty() && currentFrame < imageList.size()) {
+            QPixmap pixmap(imageList[currentFrame]);
+            if (!pixmap.isNull()) {
+                // 이미지 레이블 크기에 맞게 스케일링
+                QPixmap scaledPixmap = pixmap.scaled(
+                    videoView->size(),
+                    Qt::KeepAspectRatio,
+                    Qt::SmoothTransformation
+                    );
+                videoView->setPixmap(scaledPixmap);
+            }
+        }
+        QMainWindow::resizeEvent(event);
+    }
 };
